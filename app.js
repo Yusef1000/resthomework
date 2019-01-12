000a6f46c4c7afd7360d9eca3e857e8f795982b3
yusef <yusef.abdulla@yahoo.com>
2019-01-12 18:07:58 -0500

    adding procfile for heroku

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/api/login', (req, res) => {
 });
 
 // PORT NUMBER------------------------------------------------------------------
-let port = 1234;
+let port = process.env.PORT || 1234;
 
 app.listen(port, () => {
   console.log('Server up and running on port ' + port);
